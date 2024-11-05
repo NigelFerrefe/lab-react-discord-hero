4a0385fc8409b89b741bd1aab6fdca88fb4e6c5a
Nigel Ferreres <nferrefe@gmail.com>
2024-11-05 09:11:56 +0100

    .

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,15 +1,14 @@
-
-import './App.css';
-import Navbar from './Navbar';
-import ContainerHero from './ContainerHero';
-import BackgroundImage from './assets/discord-background.png'
+import "./App.css";
+import Navbar from "./Navbar";
+import ContainerHero from "./ContainerHero";
+import BackgroundImage from "./assets/discord-background.png";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ContainerHero />
-      <img className='bg-image' src={BackgroundImage} alt="bg image" />
+      <img className="bg-image" src={BackgroundImage} alt="bg image" />
     </div>
   );
 }
